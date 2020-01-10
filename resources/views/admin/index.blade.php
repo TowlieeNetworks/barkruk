e8c9d1507f7632481ddd4708743a05329bfb65f6
Stijn van Dongen <Stijn_vandongen@outlook.com>
2020-01-10 14:33:40 +0100

    adminpage heeft alle dashboards

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,7 +7,40 @@
         </div>
     @endif
 
-    <h1>welkom op het admin dashboard</h1>
 
-    <a href="{{ route('admin.create') }}">maak een nieuwe klant aan</a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard -> Barroc_Intense</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                            <h1>welkom op het admin dashboard</h1>
+
+
+                        You are logged in!
+
+                            <div>
+                                <a href="{{ route('admin.create') }}" class="btn btn-primary">maak een nieuwe klant aan</a>
+                            </div>
+
+                        <div>
+                            <a href="{{ route('sales.index') }}" class="btn btn-primary">Sales Dashboard</a>
+                            <a href="{{ route('finance.index') }}" class="btn btn-primary">Finance Dashboard</a>
+                            <a href="{{ route('maintenance.index') }}" class="btn btn-primary">Maintenance Dashboard</a>
+                            <a href="{{ route('head_maintenance.index') }}" class="btn btn-primary">Head Maintenance Dashboard</a>
+                            <a href="{{ route('inkoop.index') }}" class="btn btn-primary">Inkoop Dashboard</a>
+                            <a href="{{ route('customer.index') }}" class="btn btn-primary">Customer Dashboard</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,14 +15,6 @@
                     @endif
 
                     You are logged in!
-<div>
-                        <a href="{{ route('sales.index') }}" class="btn btn-primary">Sales Dashboard</a>
-                        <a href="{{ route('finance.index') }}" class="btn btn-primary">Finance Dashboard</a>
-                        <a href="{{ route('maintenance.index') }}" class="btn btn-primary">Maintenance Dashboard</a>
-                        <a href="{{ route('head_maintenance.index') }}" class="btn btn-primary">Head Maintenance Dashboard</a>
-                        <a href="{{ route('inkoop.index') }}" class="btn btn-primary">Inkoop Dashboard</a>
-                        <a href="{{ route('customer.index') }}" class="btn btn-primary">Customer Dashboard</a>
-</div>
                 </div>
             </div>
         </div>
